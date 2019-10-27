68ee0d6c57da33d94f57689e4620d1b71826bb21
Adrian Atanasov <adriansoccar@gmail.com>
2019-10-27 18:51:45 -0500

    update
    first update

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,9 +12,73 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var output: UITextField!
+    @IBOutlet weak var input: UITextField!
+    
+    struct conv{
+    var label: String
+    var input: String
+    var output: String
+    }
+    var menu = [conv(label: "Fahrenheit to celcius", input: "°F°", output: "°C"),
+                conv(label: "Celcius to Fahrenheit", input: "°C", output: "°F"),
+                conv(label: "Miles to Kilometers", input: "mi", output: "km"),
+                conv(label: "Kilometers to Miles", input: "km", output: "mi")]
 
+    @IBAction func convert(_ sender: Any) {
+        let sheet = UIAlertController(title: "Converter", message: "Choose a Converter", preferredStyle: UIAlertController.Style.actionSheet)
+       
+        sheet.addAction(UIAlertAction(title: "Fahrenheit to celcius", style: UIAlertAction.Style.default, handler: {
+            (UIAlertAction) -> Void in
+            self.output.text?.removeAll()
+            self.input.text?.removeAll()
+            self.output.textAlignment = .right
+            self.output.text?.append(self.menu[0].output)
+            self.input.textAlignment = .right
+            self.input.text?.append(self.menu[0].input)
+        }))
+        
+        sheet.addAction(UIAlertAction(title: "Celcius to Fahrenheit", style: UIAlertAction.Style.default, handler: {
+            (UIAlertAction) -> Void in
+            self.output.text?.removeAll()
+            self.input.text?.removeAll()
+            self.output.textAlignment = .right
+            self.output.text?.append(self.menu[1].output)
+            self.input.textAlignment = .right
+            self.input.text?.append(self.menu[1].input)
+            
+        }))
+        
+        sheet.addAction(UIAlertAction(title: "Miles to Kilometers", style: UIAlertAction.Style.default, handler: {
+            (UIAlertAction) -> Void in
+            self.output.text?.removeAll()
+            self.input.text?.removeAll()
+            self.output.textAlignment = .right
+            self.output.text?.append(self.menu[2].output)
+            self.input.textAlignment = .right
+            self.input.text?.append(self.menu[2].input)
+            
+        }))
 
+        sheet.addAction(UIAlertAction(title: "Kilometers to Miles", style: UIAlertAction.Style.default, handler: {
+            (UIAlertAction) -> Void in
+            self.output.text?.removeAll()
+            self.input.text?.removeAll()
+            self.output.textAlignment = .right
+            self.output.text?.append(self.menu[3].output)
+            self.input.textAlignment = .right
+            self.input.text?.append(self.menu[3].input)
+        }))
+        
+        sheet.addAction(UIAlertAction(title: "Cancel", style:
+            UIAlertAction.Style.cancel, handler: {
+            (UIAlertAction) -> Void in
+                
+        }))
+        self.present(sheet, animated: true, completion: nil)
+    }
+    
+    
 }
 
